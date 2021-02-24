1446c77619116998fbe5662471a915243a460475
ExLog <55097092+ExLog@users.noreply.github.com>
2021-02-24 19:02:36 +0800

    Added Generator component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="my-10 flex justify-center flex-col items-center ">
+    <h1 class="text-3xl text-gray-300 uppercase">Password Generator</h1>
+    <Generator />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Generator from "./components/Generator.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    Generator
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap");
+
+h1 {
+  font-family: "Montserrat";
 }
 </style>
---
+++ src/components/Generator.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="my-5 text-white">
+    <input
+      v-model="password"
+      class="px-3 rounded bg-raisin-black2 "
+      type="text"
+    />
+    <button class="mx-1" v-on:click="generatePassword()">Generate</button>
+    <div class="mx-2 my-5">
+      <label class="my-3 flex justify-start items-start">
+        <div
+          class="bg-raisin-black2 flex justify-center items-center mr-2 "
+        ></div>
+        <div class="text-gray-300 mr-5">Length</div>
+        <input
+          v-model="length"
+          type="text"
+          class="px-1 mx-1 w-20 bg-raisin-black2"
+        />
+      </label>
+      <label class="my-3 flex justify-start items-start">
+        <div
+          class="bg-raisin-black2 rounded w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-antique-ruby"
+        >
+          <input v-model="numbers" type="checkbox" class="opacity-0 absolute" />
+          <svg
+            class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none"
+            viewBox="0 0 20 20"
+          >
+            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+          </svg>
+        </div>
+        <div class="text-gray-300 select-none">Numbers</div>
+      </label>
+
+      <label class="my-3 flex justify-start items-start">
+        <div
+          class="bg-raisin-black2 rounded w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-antique-ruby"
+        >
+          <input v-model="symbols" type="checkbox" class="opacity-0 absolute" />
+          <svg
+            class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none"
+            viewBox="0 0 20 20"
+          >
+            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+          </svg>
+        </div>
+        <div class="text-gray-300 select-none">Symbols</div>
+      </label>
+
+      <label class="my-3 flex justify-start items-start">
+        <div
+          class="bg-raisin-black2 rounded w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-antique-ruby"
+        >
+          <input
+            v-model="lowercase"
+            type="checkbox"
+            class="opacity-0 absolute"
+          />
+          <svg
+            class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none"
+            viewBox="0 0 20 20"
+          >
+            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+          </svg>
+        </div>
+        <div class="text-gray-300 select-none">Lowercase</div>
+      </label>
+
+      <label class="my-3 flex justify-start items-start">
+        <div
+          class="bg-raisin-black2 rounded w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-antique-ruby"
+        >
+          <input
+            v-model="uppercase"
+            type="checkbox"
+            class="opacity-0 absolute"
+          />
+          <svg
+            class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none"
+            viewBox="0 0 20 20"
+          >
+            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+          </svg>
+        </div>
+        <div class="text-gray-300 select-none">Uppercase</div>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+import generator from "generate-password";
+export default {
+  data() {
+    return {
+      password: "",
+      length: 10,
+      numbers: false,
+      symbols: false,
+      lowercase: true,
+      uppercase: true
+    };
+  },
+  methods: {
+    generatePassword() {
+      try {
+        this.password = generator.generate({
+          length: this.length,
+          numbers: this.numbers,
+          symbols: this.symbols,
+          lowercase: this.lowercase,
+          uppercase: this.uppercase
+        });
+      } catch (TypeError) {
+        console.log("There are no checked checkboxes");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+input:checked + svg {
+  display: block;
+}
+</style>
