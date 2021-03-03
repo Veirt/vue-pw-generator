8a6fdadbedfe52a10b6e013c4b753828b66a19e6
ExLog <55097092+ExLog@users.noreply.github.com>
2021-03-03 16:34:04 +0800

    Added comment

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- This is an example component -->
+  <!-- pwgen.exlog.wtf -->
   <div class="w-full h-screen bg-center bg-no-repeat bg-cover bg-raisin-black">
     <div
       class="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center"
